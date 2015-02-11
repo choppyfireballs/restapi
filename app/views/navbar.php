100df3d4b596bec66529e134f748e6f13a99dfd4
david roszkowski <trumpetcrazy@gmail.com>
2015-02-10 22:00:39 -0600

    added navbar

---
+++ app/views/navbar.php
@@ -0,0 +1,21 @@
+<?php echo $_SERVER['HTTP_HOST'];?>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+		<?php echo "<a class='navbar-brand' href='http://".$_SERVER['HTTP_HOST']."'>Home</a>";?>
+            </div>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav" id="bs-example-navbar-collapse-1">
+                    <li>
+                        <?php echo "<a href='http://".$_SERVER['HTTP_HOST']."/sudoku/public/sudoku'>Sudoku <span class='sr-only'></span></a>";?>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
