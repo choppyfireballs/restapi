931fb7255993bb118452b05a339659a335f349e6
david roszkowski <trumpetcrazy@gmail.com>
2015-02-11 23:23:27 -0600

    fixed include

---
+++ app/views/includes.php
@@ -0,0 +1,3 @@
+<?php
+echo "<link rel='stylesheet' href='http://".Request::server('HTTP_HOST')."/css/bootstrap.css'>";
+echo "<link rel='stylesheet' href='http://localhost/sudoku/public/css/custom.css'>";
